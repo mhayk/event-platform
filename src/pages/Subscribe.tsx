fec82ed8ad37747d14f8637e1de63389bf8f4811
Mhayk Whandson <hi@mhayk.com>
2022-06-27 13:40:42 +0100

    feat: form is working with graphql mutation

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,39 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 
+const CREATE_SUBSCRIBE_MUTATION = gql`
+    mutation CreateSubscriber ($name: String!, $email: String!) {
+        createSubscriber(data: {name: $name, email: $email}) {
+            id
+        }
+    }
+
+`
+
 export function Subscribe() {
+
+    const navigate = useNavigate()
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const [createSubscribe, { data, loading }] = useMutation(CREATE_SUBSCRIBE_MUTATION)
+
+    async function handleSubscribe(event: FormEvent) {
+        event.preventDefault()
+
+        await createSubscribe({
+            variables: {
+                name,
+                email
+            }
+        })
+
+        navigate('/event')
+    }
+
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
             <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
@@ -16,21 +49,27 @@ export function Subscribe() {
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded">
                     <strong className="text-2xl mb-6 block">Sign up for free</strong>
 
-                    <form action="" className="flex flex-col gap-2 w-full">
+                    <form
+                        onSubmit={handleSubscribe}
+                        className="flex flex-col gap-2 w-full"
+                    >
                         <input
                             className="bg-gray-900 rounded px-5 h-14"
                             type="text"
                             placeholder="Your full name"
+                            onChange={event => setName(event.target.value)}
                         />
                         <input
                             className="bg-gray-900 rounded px-5 h-14"
                             type="text"
                             placeholder="Type your e-mail"
+                            onChange={event => setEmail(event.target.value)}
                         />
 
                         <button
                             type="submit"
-                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                            disabled={loading}
+                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
                         >
                             Get my spot
                         </button>
